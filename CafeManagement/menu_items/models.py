aafe965e63a7943e0389fc5b06cd07fd546c5d0d
VahidM93 <vahid9068@gmail.com>
2023-06-25 22:50:39 +0330

    comment models created partially due to AccountApp

--- CafeManagement/menu_items/models.py
+++ CafeManagement/menu_items/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+# from accounts.models import Account
 
 class Category(models.Model):
     name=models.CharField(max_length=150)
     Category_id=models.IntegerField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 class Discount(models.Model):
     start_date=models.DateTimeField(auto_now=True)
@@ -15,17 +19,16 @@ class Discount(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.description,self.percent
 
-
-class Comment(models.Model):
-    pass
 
 class MenuItem(models.Model):
   
     name=models.CharField(max_length=150)
     price=models.IntegerField()
-    Category_id=models.ManyToManyField(Category)
-    discount_id=models.ManyToManyField(Discount)
+    Category_id=models.ForeignKey(Category,on_delete=models.CASCADE)
+    discount_id=models.ForeignKey(Discount,on_delete=models.CASCADE)
     period_time_service=models.IntegerField()
     estimated_time_service=models.IntegerField()
     created_at=models.DateTimeField(auto_now_add=True)
@@ -35,3 +38,13 @@ class MenuItem(models.Model):
     def __str__(self):
             return self.name
 
+
+class Comment(models.Model):
+    text=models.TextField(max_length=500)
+    # user_id=models.ForeignKey(Account,on_delete=models.SET_NULL,null=True)
+    menuItem_id=models.ForeignKey()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user_id, self.text
