20cca0d8a91cdf43084cdd7eecc1b48822109ce2
VahidM93 <vahid9068@gmail.com>
2023-06-25 21:17:17 +0330

    model Category was registered

--- CafeManagement/menu_items/admin.py
+++ CafeManagement/menu_items/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import MenuItem
+from .models import MenuItem,Category
 
 admin.site.register(MenuItem)
+admin.site.register(Category)
--- CafeManagement/menu_items/models.py
+++ CafeManagement/menu_items/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Category(models.Model):
-    pass
-
+    name=models.CharField(max_length=150)
+    Category_id=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
 class Discount(models.Model):
     pass
