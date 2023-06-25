d1fd14fcd91e071c7dcc27a5f7a2f27b8cb945db
kiana alamy <as.faraso.97@gmail.com>
2023-06-26 00:36:03 +0430

    class Order

--- CafeManagement/orders/models.py
+++ CafeManagement/orders/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 class Order(models.Model):
-    pass
+    table_id= models.IntegerField()
+    status= models.BooleanField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    type_order= models.CharField(max_length=2)
+    user_id= models.ForeignKey(account , on_delete= models.CASCADE)
 
 
 class Reciept(models.Model):
