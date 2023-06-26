603228a12472b2430caad0617292e70a1353838b
Alireza Rezapour <alireza.rezapour16@gmail.com>
2023-06-26 21:29:57 +0330

    home path added.

--- CafeManagement/home/urls.py
+++ CafeManagement/home/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('index.html', views.home, name='home'),
+    path('menu.html', views.menu, name='menu'),
+    path('about.html', views.about, name='about'),
+    path('book.html', views.book, name='book'),
 ]
+
