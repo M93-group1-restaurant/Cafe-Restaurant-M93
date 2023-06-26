6774e9d16ae66685bfe7c65a18d414f21a9a5b04
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-06-27 02:01:32 +0330

    move urls to other apps

--- CafeManagement/home/urls.py
+++ CafeManagement/home/urls.py
@@ -1,11 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('menu/', views.menu, name='menu'),
     path('about/', views.about, name='about'),
-    path('book/', views.book, name='book'),
 ]
 
