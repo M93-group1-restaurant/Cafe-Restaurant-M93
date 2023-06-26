7b78e4670c0fabf31e88ab2522b90fcb4acc7a87
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-06-27 02:05:45 +0330

    move views to other apps

--- CafeManagement/home/views.py
+++ CafeManagement/home/views.py
@@ -4,11 +4,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
-def menu(request):
-    return render(request, 'menu.html')
 
 def about(request):
     return render(request, 'about.html')
 
-def book(request):
-    return render(request, 'book.html')
