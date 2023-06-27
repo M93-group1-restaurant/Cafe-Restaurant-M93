fdebe760c70b5d2900658f9142cf77cdd318b5d4
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-06-27 10:01:54 +0330

    category foreinkey parent category

--- CafeManagement/menu_items/models.py
+++ CafeManagement/menu_items/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     name = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    parent_category = models.ForeignKey(Category, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
 
