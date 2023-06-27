27f2fb4aaa73b4824e62a63231cd70ef15251102
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-06-28 01:38:00 +0330

    add view for booking a table

--- CafeManagement/orders/views.py
+++ CafeManagement/orders/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .forms import BookTableForm
 
 
 def book(request):
-    return render(request, "book.html")
+    if request.method=='POST':
+        form=BookTableForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+            print(data)
+    else:
+        form=BookTableForm()
+    return render(request, "book.html", context={"form":form})
