e7442f39a7773c28325b3696c4b99855391d54d1
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-27 12:42:39 +0330

    modify models to fallow best practise

--- CafeManagement/menu_items/models.py
+++ CafeManagement/menu_items/models.py
@@ -1,51 +1,35 @@
 from django.db import models
 
-# from accounts.models import Account
 
-
-class Category(models.Model):
+class ModelInfo(models.Model):
     name = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    parent_category = models.ForeignKey("Category", on_delete=models.CASCADE, null=True,blank=True)
+    slug = models.SlugField()
+
+    class Mete:
+        abstract = True
+        ordering = ('-updated_at', '-created_at')
+
     def __str__(self):
         return self.name
 
 
-class Discount(models.Model):
-    start_date = models.DateTimeField(auto_now=True)
-    end_date = models.DateTimeField(auto_now=True)
-    percent = models.IntegerField()
-    description = models.CharField(max_length=150)
-    number = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class Category(ModelInfo):
+    parent_category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="categories", null=True,
+                                        blank=True)
 
-    def __str__(self):
-        return self.description, self.percent
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 
-class MenuItem(models.Model):
-    name = models.CharField(max_length=150)
+class MenuItem(ModelInfo):
     price = models.IntegerField()
-    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    discount = models.ForeignKey(Discount, on_delete=models.CASCADE)
-    period_time_service = models.IntegerField()
-    estimated_time_service = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to="images/")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="menuItems")
+    period_time_service = models.DurationField()
+    estimated_cooking_time = models.DurationField()
+    image = models.ImageField(upload_to="images/", default='no_image.jpg', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.name
-
-
-class Comment(models.Model):
-    text = models.TextField(max_length=500)
-    # user=models.ForeignKey(Account,on_delete=models.SET_NULL,null=True)
-    menuItem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user, self.text
+    class Meta:
+        verbose_name_plural = 'MenuItems'
