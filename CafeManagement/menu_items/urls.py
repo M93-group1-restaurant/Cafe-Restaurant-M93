dd86178f5a29e6a5b878b1e4888edf883e9809df
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-06-28 17:10:08 +0330

    clean codes,

--- CafeManagement/menu_items/urls.py
+++ CafeManagement/menu_items/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path("menu/", views.menu, name="menu"),
