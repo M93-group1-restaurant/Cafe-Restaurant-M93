e050a8a355424e998d9e10b559e19b1ed27b42b1
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-06-29 21:02:19 +0330

    order model verified

--- CafeManagement/orders/models.py
+++ CafeManagement/orders/models.py
@@ -27,13 +27,14 @@ class Order(ModelInfo):
         SEND = "S", "SEND"
         EAT = "E", "EAT"
 
-    table_ordered = models.ForeignKey(
-        Table, on_delete=models.SET_NULL, related_name="orders", null=True, blank=True
+    table = models.ForeignKey(
+        "Table", on_delete=models.SET_NULL, related_name="orders", null=True, blank=True
     )
-    delivery_status = models.CharField(choices=DeliveryChoice.choices, max_length=1)
+    delivery_status = models.CharField(
+        choices=DeliveryChoice.choices, max_length=20)
     start_reserve_date = models.DateTimeField(null=True, blank=True)
     end_reserve_date = models.DateTimeField(null=True, blank=True)
-    user_session = models.ForeignKey(
+    userSession = models.ForeignKey(
         UserSession,
         on_delete=models.SET_NULL,
         related_name="orders",
@@ -46,7 +47,7 @@ class Order(ModelInfo):
 
 
 class Receipt(ModelInfo):
-    related_order = models.OneToOneField(
+    order = models.OneToOneField(
         Order, on_delete=models.SET_NULL, related_name="receipt", null=True, blank=True
     )
     total_price = models.IntegerField()
@@ -57,14 +58,14 @@ class Receipt(ModelInfo):
 
 
 class Order_menuItem(ModelInfo):
-    menu_item = models.ForeignKey(
+    menuItem = models.ForeignKey(
         MenuItem,
         on_delete=models.SET_NULL,
         related_name="orders",
         null=True,
         blank=True,
     )
-    related_order = models.ForeignKey(
+    order = models.ForeignKey(
         Order, on_delete=models.CASCADE, related_name="menuItems"
     )
     quantity = models.PositiveIntegerField()
