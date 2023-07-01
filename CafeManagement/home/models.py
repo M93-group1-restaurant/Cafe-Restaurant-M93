a30ac8b7345040a887ca315650ece515e0827a1a
Alireza Rezapour <alireza.rezapour16@gmail.com>
2023-07-01 23:56:43 +0330

    BUG FIX

--- CafeManagement/home/models.py
+++ CafeManagement/home/models.py
@@ -1,19 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Customer(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.TextField()
-    contact = models.CharField(max_length = 10)
-    orders = models.IntegerField(default=0)
-    total_sale = models.IntegerField(default=0)
+    pending = 'Pending'
+    verified = 'Verified'
 
     STATUS = (
         (pending,pending),
         (verified,verified),
     )
 
-    pending = 'Pending'
-    verified = 'Verified'
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.TextField()
+    contact = models.CharField(max_length = 10)
+    orders = models.IntegerField(default=0)
+    total_sale = models.IntegerField(default=0)
 
     def __str__(self):
         return self.customer.first_name + " " + self.customer.last_name
