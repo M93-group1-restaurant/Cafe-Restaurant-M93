f4bb1fdafd22ff2780e53dc0763b66305375856b
Alireza Rezapour <alireza.rezapour16@gmail.com>
2023-07-01 23:23:39 +0330

    customer class relocated.

--- CafeManagement/home/models.py
+++ CafeManagement/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.TextField()
+    contact = models.CharField(max_length = 10)
+    orders = models.IntegerField(default=0)
+    total_sale = models.IntegerField(default=0)
+
+    STATUS = (
+        (pending,pending),
+        (verified,verified),
+    )
+
+    pending = 'Pending'
+    verified = 'Verified'
+
+    def __str__(self):
+        return self.customer.first_name + " " + self.customer.last_name
