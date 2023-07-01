97e6b5397b3e27a90fdc495b2eac964690998910
Alireza Rezapour <alireza.rezapour16@gmail.com>
2023-07-01 23:36:33 +0330

    password added

--- CafeManagement/home/forms.py
+++ CafeManagement/home/forms.py
@@ -5,3 +5,4 @@ class SignUpForm(forms.ModelForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email', 'required':'true'}))
     firstname = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name', 'required':'true'}))
     lastname = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name', 'required':'true'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password', 'required':'true'}))
