3d11b70adf828fec1ca397af66b981a47788d078
Alireza Rezapour <alireza.rezapour16@gmail.com>
2023-07-01 23:25:27 +0330

    add user url added.

--- CafeManagement/menu_items/models.py
+++ CafeManagement/menu_items/models.py
@@ -2,24 +2,6 @@ from django.db import models
 from datetime import timedelta
 from core.models import ModelInfo
 
-class Customer(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.TextField()
-    contact = models.CharField(max_length = 10)
-    orders = models.IntegerField(default=0)
-    total_sale = models.IntegerField(default=0)
-
-    STATUS = (
-        (pending,pending),
-        (verified,verified),
-    )
-
-    pending = 'Pending'
-    verified = 'Verified'
-
-    def __str__(self):
-        return self.customer.first_name + " " + self.customer.last_name
-        
 class Category(ModelInfo):
     name = models.CharField(max_length=150)
     parent_category = models.ForeignKey(
