ffe1ac5d0043665fa1a31b7fc05bd5d1b62c7a95
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-02 02:24:22 +0330

    initialize xiew and form for cart

--- CafeManagement/orders/views.py
+++ CafeManagement/orders/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render, redirect
-from .forms import BookTableForm
+from .forms import CartForm, BookTableForm
 from home.models import RestaurantInfo
+
+
+def cart(request):
+    info = RestaurantInfo.objects.first()
+    if request.method == "POST":
+        form = CartForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            print(data)
+        return redirect("home")
+    else:
+        form = CartForm()
+    return render(request, "cart.html", context={"form":form, "info": info})
 
 
 def book(request):
