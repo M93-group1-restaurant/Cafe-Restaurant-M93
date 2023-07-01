6f64330f0301a0782e018e8c21cc61950b3090da
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-01 14:13:27 +0330

    add __str__ to models

--- CafeManagement/core/models.py
+++ CafeManagement/core/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 
 class ModelInfo(models.Model):
-    ...
+    class Meta:
+        abstract = True
 
 
 class AboutContent(ModelInfo):
     title = models.CharField(max_length=50)
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 
 class RestaurantInfo(ModelInfo):
@@ -18,6 +22,9 @@ class RestaurantInfo(ModelInfo):
     email = models.EmailField()
     opening_hours = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.title
+
 
 class SliderContent(ModelInfo):
     title = models.CharField(max_length=50)
@@ -27,3 +34,6 @@ class SliderContent(ModelInfo):
     RestaurantInfo = models.ForeignKey(
         RestaurantInfo, on_delete=models.SET_NULL, related_name="sliders"
     )
+
+    def __str__(self):
+        return self.title
