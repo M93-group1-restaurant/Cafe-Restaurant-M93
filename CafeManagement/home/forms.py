07b8540c07a158f36fbe826eb88d87e7ff5e2e49
Alireza Rezapour <alireza.rezapour16@gmail.com>
2023-07-01 23:42:40 +0330

    pep8

--- CafeManagement/home/forms.py
+++ CafeManagement/home/forms.py
@@ -1,18 +1,26 @@
 from django import forms
 from .models import *
 
+
 class SignUpForm(forms.ModelForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email', 'required':'true'}))
-    firstname = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name', 'required':'true'}))
-    lastname = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name', 'required':'true'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password', 'required':'true'}))
-    confirmpass = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Repeat Password', 'required':'true'}))
-    address = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Address', 'required':'true'}))
-    contact = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Contact', 'required':'true'}))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'Email', 'required': 'true'}))
+    firstname = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'First Name', 'required': 'true'}))
+    lastname = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Last Name', 'required': 'true'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Password', 'required': 'true'}))
+    confirmpass = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Repeat Password', 'required': 'true'}))
+    address = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Address', 'required': 'true'}))
+    contact = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Contact', 'required': 'true'}))
 
     class meta:
-        model : User
-        fields = ['email','firstname','lastname','password','confirmpass']
+        model: User
+        fields = ['email', 'firstname', 'lastname', 'password', 'confirmpass']
 
     def clean(self):
         cleaned_data = super(SignUpForm, self).clean()
@@ -20,4 +28,5 @@ class SignUpForm(forms.ModelForm):
         confirm_pass = cleaned_data.get('confirmpass')
 
         if password != confirm_pass:
-            raise forms.ValidationError('Password and Confirm Password does not match!')
+            raise forms.ValidationError(
+                'Password and Confirm Password does not match!')
