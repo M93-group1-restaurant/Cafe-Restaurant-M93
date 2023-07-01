27fa55ee97d3f441abb28a52d7a6c40039c48ebc
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-01 14:02:24 +0330

    set slider forein key to restaaurantinfo

--- CafeManagement/core/models.py
+++ CafeManagement/core/models.py
@@ -5,25 +5,25 @@ class ModelInfo(models.Model):
     ...
 
 
-class SliderContent(ModelInfo):
-    title=models.CharField(max_length=50)
-    content=models.TextField()
-    button_text=models.CharField(max_length=20)
-    button_link=models.CharField(max_length=50)
-
-
 class AboutContent(ModelInfo):
-    title=models.CharField(max_length=50)
-    content=models.TextField()
+    title = models.CharField(max_length=50)
+    content = models.TextField()
 
 
 class RestaurantInfo(ModelInfo):
     title
     about
-    slider
     phone_number
     address
     email
     opening_hours
-    
-    
+
+
+class SliderContent(ModelInfo):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    button_text = models.CharField(max_length=20)
+    button_link = models.CharField(max_length=50)
+    RestaurantInfo = models.ForeignKey(
+        on_delete=models.SET_NULL, related_name="sliders"
+    )
