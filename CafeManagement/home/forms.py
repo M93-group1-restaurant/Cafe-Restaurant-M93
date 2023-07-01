ae403a7e0607476a0c2254d52b9f95287653fd4b
Alireza Rezapour <alireza.rezapour16@gmail.com>
2023-07-01 23:35:39 +0330

    email attr added

--- CafeManagement/home/forms.py
+++ CafeManagement/home/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from .models import *
 
 class SignUpForm(forms.ModelForm):
-    pass
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email', 'required':'true'}))
