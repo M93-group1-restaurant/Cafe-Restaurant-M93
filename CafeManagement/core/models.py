b2270f6219c791bca2a003e649fb0bd14004d7e7
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-01 13:49:07 +0330

    impl base of models in core/models

--- CafeManagement/core/models.py
+++ CafeManagement/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class ModelInfo(models.Model):
+    ...
+
+
+class SliderContent(ModelInfo):
+    title
+    content
+    button_text
+    button_link
+
+
+class AboutContent(ModelInfo):
+    title
+    content
+
+
+class RestaurantInfo(ModelInfo):
+    title
+    about
+    slider
+    phone_number
+    address
+    email
+    opening_hours
+    
+    
