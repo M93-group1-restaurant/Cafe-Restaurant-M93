e8d58bda5e7b17bf67f446e188de14c381c23d2f
Alireza Rezapour <alireza.rezapour16@gmail.com>
2023-07-01 23:25:43 +0330

    add user url added

--- CafeManagement/home/urls.py
+++ CafeManagement/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    url(r'^dashboard/admin/users/add_user/$', views.add_user, name='add_user'),
 ]
