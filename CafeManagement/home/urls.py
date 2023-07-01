32684b954cb37ba7ae500bab5051a89a4d263997
Alireza Rezapour <alireza.rezapour16@gmail.com>
2023-07-02 00:01:38 +0330

    path instead of url

--- CafeManagement/home/urls.py
+++ CafeManagement/home/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    url(r'^dashboard/admin/users/add_user/$', views.add_user, name='add_user'),
+    path("dashboard/admin/users/add_user/", views.add_user, name='add_user'),
 ]
