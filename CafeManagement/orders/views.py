3f03218fd46798d21f4f8f0782c27fb5472e72a3
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-03 00:57:31 +0330

    create order object after confirmation using cooki

--- CafeManagement/orders/views.py
+++ CafeManagement/orders/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import CartForm, BookTableForm
 from home.models import RestaurantInfo
+from .models import Order_menuItem, Order
+from menu_items.models import MenuItem
 import json
 
 
@@ -8,9 +10,17 @@ def cart(request):
     if request.method == "POST":
         try:
             order_items=json.loads(request.COOKIES["cart"])
+            order=Order.objects.create()
+            for (i,j) in order_items.items():
+                menuItem = MenuItem.objects.get(id=i)
+                Order_menuItem.objects.create(menuItem=menuItem, order=order, quantity=j['quantity'])
         except:
             order_items={}
-        print(order_items)
+        
+        
+        
+        
+        redirect('home')
 
     info = RestaurantInfo.objects.first()
     return render(request, "cart.html", context={"info": info})
