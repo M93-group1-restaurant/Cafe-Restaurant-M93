0dfe2a277b9e48268bb805394129e078397fa593
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-02 14:25:49 +0330

    add cart.js to base.html

--- CafeManagement/orders/views.py
+++ CafeManagement/orders/views.py
@@ -5,15 +5,15 @@ from home.models import RestaurantInfo
 
 def cart(request):
     info = RestaurantInfo.objects.first()
-    if request.method == "POST":
-        form = CartForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            print(data)
-        return redirect("home")
-    else:
-        form = CartForm()
-    return render(request, "cart.html", context={"form":form, "info": info})
+    # if request.method == "POST":
+    #     form = CartForm(request.POST)
+    #     if form.is_valid():
+    #         data = form.cleaned_data
+    #         print(data)
+    #     return redirect("home")
+    # else:
+    #     form = CartForm()
+    return render(request, "cart.html", context={"info": info})
 
 
 def book(request):
