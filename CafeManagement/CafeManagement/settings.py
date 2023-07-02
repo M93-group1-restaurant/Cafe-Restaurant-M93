ef189a4d146d0001577c7eca1e3dc5c0f7599a5f
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-02 11:03:38 +0330

    deploy project to liara

--- CafeManagement/CafeManagement/settings.py
+++ CafeManagement/CafeManagement/settings.py
@@ -81,8 +81,7 @@ WSGI_APPLICATION = "CafeManagement.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
     }
 }
 
