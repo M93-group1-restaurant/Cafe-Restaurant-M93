c1a015dd13acd5c0690d06514a10b79c89af4dd8
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-04 06:36:09 +0330

    customize header in panel admin

--- CafeManagement/home/urls.py
+++ CafeManagement/home/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
+# Django admin customization
+admin.site.header = "Caffe Restaurant"
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
