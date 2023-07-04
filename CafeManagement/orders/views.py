3ac5f0292913a5e18a76d2456f9c925e0084d67b
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-04 11:47:59 +0330

    use reduce instead of for

--- CafeManagement/orders/views.py
+++ CafeManagement/orders/views.py
@@ -12,19 +12,18 @@ def cart(request):
         order_items = json.loads(request.COOKIES["cart"])
         menuItems=[(MenuItem.objects.get(id=i),j["quantity"]) for i,j in order_items.items()]
         total_price=0
-        for menu_item in menuItems:
-            total_price+=menu_item[0].price*menu_item[1]
-            # total_price = reduce(lambda x,y : x[0].price*x[1]+y[0].price*y[1],menuItems)
+        # for menu_item in menuItems:
+        #     total_price+=menu_item[0].price*menu_item[1]
+        total_price = reduce(lambda x,y : x[0].price*x[1]+y[0].price*y[1],menuItems)
     except:
         order_items = {}
         menuItems=()
         total_price=0
     if request.method == "POST":
         order = Order.objects.create()
-        for (i, j) in order_items.items():
-            menuItem = MenuItem.objects.get(id=i)
+        for menuItem in menuItems:
             Order_menuItem.objects.create(
-                menuItem=menuItem, order=order, quantity=j["quantity"]
+                menuItem=menuItem[0], order=order, quantity=menuItem[1]
             )
         return redirect("home")
     info = RestaurantInfo.objects.first()
