bcd368d2239d668e5297817b9027891f0f7134e3
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-04 13:04:05 +0330

    add search_fields to orders

--- CafeManagement/orders/admin.py
+++ CafeManagement/orders/admin.py
@@ -5,18 +5,22 @@ from .models import Order, Receipt, Table, Order_menuItem
 class TableAdmin(admin.ModelAdmin):
     fields = ('number', 'space_position', 'capacity')
     list_display = ('number', 'capacity', 'space_position')
+    search_fields = ('number',)
 
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('table', 'serving_status',)
+    search_fields = ('order', 'phone_number', 'serving_status')
 
 
 class ReceiptAdmin(admin.ModelAdmin):
     list_display = ('order', 'total_price', 'final_price')
+    search_fields = ('order',)
 
 
 class Order_menuItemAdmin(admin.ModelAdmin):
     list_display = ('menuItem', 'quantity')
+    search_fields = ('menuItem',)
 
 
 admin.site.register(Table, TableAdmin)
