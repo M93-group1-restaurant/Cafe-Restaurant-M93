5c6ad707308bcfbef4ca8b913d592cf28323d09d
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-04 08:38:58 +0330

    impl CategoryAdmin

--- CafeManagement/menu_items/admin.py
+++ CafeManagement/menu_items/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import MenuItem, Category
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+
+
+
 admin.site.register(MenuItem)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
