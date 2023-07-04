5f1c44cea291e4a4543b784286535af29e1f7874
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-04 13:17:16 +0330

    add cart form for ordering

--- CafeManagement/orders/forms.py
+++ CafeManagement/orders/forms.py
@@ -2,7 +2,19 @@ from django import forms
 
 
 class CartForm(forms.Form):
-    ...
+    phone_number=forms.CharField(
+        widget=forms.TextInput(
+            attrs={"placeholder": "Your phone number", "class": "form-control"}
+        ),
+        label="",
+    )
+    table_number=forms.IntegerField(
+        widget=forms.TextInput(
+            attrs={"placeholder": "Table number", "class": "form-control"}
+        ),
+        label="",
+        required=False,
+    )
 
 
 class BookTableForm(forms.Form):
