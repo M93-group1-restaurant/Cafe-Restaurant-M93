dffeaab7dff01a97e44ce3c3426f73cad25b0688
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-04 12:13:50 +0330

    impl image_preview to show image in admin panel

--- CafeManagement/menu_items/admin.py
+++ CafeManagement/menu_items/admin.py
@@ -12,13 +12,17 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('price', 'category', 'less_description',)
+    list_display = ('image_preview', 'price', 'category', 'less_description',)
     list_display_links = ('less_description',)
 
     @admin.display(description=None)
     def less_description(self, obj):
         return format_html(f'<span style="color:green">{obj.description[:20]}</span>')
 
+    def image_preview(self, obj):
+        return format_html('<img src="{}" style="max-height: 200px; max-width: 200px;" />'.format(obj.image.url))
+
+    image_preview.short_description = 'Image Preview'
 
 
 admin.site.register(MenuItem, MenuItemAdmin)
