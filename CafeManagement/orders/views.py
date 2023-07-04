0fb8a9fbd3786da25283329da6a74b0a8e6ad56f
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-04 12:12:38 +0330

    delete cart cookie after order submition

--- CafeManagement/orders/views.py
+++ CafeManagement/orders/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from .forms import CartForm, BookTableForm
 from home.models import RestaurantInfo
 from .models import Order_menuItem, Order
 from menu_items.models import MenuItem
 import json
-from functools import reduce
 
 
 def cart(request):
@@ -12,20 +13,22 @@ def cart(request):
         order_items = json.loads(request.COOKIES["cart"])
         menuItems=[(MenuItem.objects.get(id=i),j["quantity"]) for i,j in order_items.items()]
         total_price=0
-        # for menu_item in menuItems:
-        #     total_price+=menu_item[0].price*menu_item[1]
-        total_price = reduce(lambda x,y : x[0].price*x[1]+y[0].price*y[1],menuItems)
+        for menu_item in menuItems:
+            total_price+=menu_item[0].price*menu_item[1]
     except:
         order_items = {}
         menuItems=()
         total_price=0
+
     if request.method == "POST":
         order = Order.objects.create()
         for menuItem in menuItems:
             Order_menuItem.objects.create(
                 menuItem=menuItem[0], order=order, quantity=menuItem[1]
             )
-        return redirect("home")
+        response = HttpResponseRedirect(reverse("home"))
+        response.delete_cookie("cart")
+        return response
     info = RestaurantInfo.objects.first()
     return render(request, "cart.html", context={"info": info, "menuItems": menuItems, "total_price":total_price})
 
