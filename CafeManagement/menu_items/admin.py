5be3f7a528f02daa401fb567fcb69b71c3a2d373
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-07-04 12:12:24 +0330

    mdfy to have less description

--- CafeManagement/menu_items/admin.py
+++ CafeManagement/menu_items/admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
 from .models import MenuItem, Category
+from django.utils.html import format_html
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    exclude = ()
     list_display = ('name',)
 
 
+# def show_image(obj):
+#     return format_html(f'<img src="obj.image" alt="sss" width="500" height="600">')
+
+
 class MenuItemAdmin(admin.ModelAdmin):
-    exclude = ()
-    list_display = ('image', 'price', 'category')
+    list_display = ('price', 'category', 'less_description',)
+    list_display_links = ('less_description',)
+
+    @admin.display(description=None)
+    def less_description(self, obj):
+        return format_html(f'<span style="color:green">{obj.description[:20]}</span>')
+
 
 
 admin.site.register(MenuItem, MenuItemAdmin)
 admin.site.register(Category, CategoryAdmin)
-
-
