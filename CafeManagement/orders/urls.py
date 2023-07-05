1444332c04440261e5ba799cab98801d34112556
kiana alamy <as.faraso.97@gmail.com>
2023-07-05 10:59:57 +0430

    add customer url to orders app

--- CafeManagement/orders/urls.py
+++ CafeManagement/orders/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("cart/", views.CartView.as_view(), name="cart"),
+    path("customer/", views.CustomerView.as_view(), name="customer"),
     path("book/", views.BookView.as_view(), name="book"),
 ]
