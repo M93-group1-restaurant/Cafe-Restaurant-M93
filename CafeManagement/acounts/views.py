86a491e93d64fbba1bfa78fa06ce61f073bc1c2e
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-10 14:56:27 +0330

    add render to views

--- CafeManagement/acounts/views.py
+++ CafeManagement/acounts/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views import View
+from orders.models import Order, Receipt
+from menu_items.models import MenuItem
 
 
 class CashierView(LoginRequiredMixin, PermissionRequiredMixin, View):
-    
+    # permission_required = "acounts."
     def get(self, request):
-        ...
+        orders = Order.objects.all()
+        menuItems = MenuItem.objects.all()
+        return render(request, 'cashier.html', context={"orders":orders, "menuItems": menuItems})
     
     def post(self, request):
         ...
 
 
 class ManagerView(LoginRequiredMixin, PermissionRequiredMixin, View):
-
+    # permission_required = "acounts."
     def get(self, request):
-        ...
+        reciepts = Receipt.objects.all()
+        return render(request, 'manager.html', context={"reciepts":reciepts})
 
