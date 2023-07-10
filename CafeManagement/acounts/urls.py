6aa446193634493b56140d8c06bb25b06ccffefb
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-11 01:25:19 +0330

    add login logout urls

--- CafeManagement/acounts/urls.py
+++ CafeManagement/acounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
     path("cashier/", views.CashierView.as_view(), name="cashier"),
     path("manager/", views.ManagerView.as_view(), name="manager"),
 ]
