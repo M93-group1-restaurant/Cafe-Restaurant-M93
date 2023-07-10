e84fdbde04bdb7be77f42f1a08a884ac11884a66
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-10 12:45:42 +0330

    add login_required decorator to views

--- CafeManagement/acounts/views.py
+++ CafeManagement/acounts/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.views import View
 
-
+@login_required
 class CashierView(View):
     
-    def get(self):
+    def get(self, request):
         ...
     
-    def post(self):
+    def post(self, request):
         ...
 
-
+@login_required
 class ManagerView(View):
 
-    def get(self):
+    def get(self, request):
         ...
 
