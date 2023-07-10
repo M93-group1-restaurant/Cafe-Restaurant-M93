ffc084a69f7f98a20ded499ea3e6a7ee4f9e01bb
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-10 21:27:44 +0330

    change managerview

--- CafeManagement/acounts/admin.py
+++ CafeManagement/acounts/admin.py
@@ -22,7 +22,7 @@ class CustomUserAdmin(UserAdmin):
         (None, {"fields": ("phone_number", "password")}),
         (
             "Permissions",
-            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions", "is_superuser")},
         ),
     )
     add_fieldsets = (
@@ -38,6 +38,7 @@ class CustomUserAdmin(UserAdmin):
                     "is_active",
                     "groups",
                     "user_permissions",
+                    "is_superuser"
                 ),
             },
         ),
--- CafeManagement/acounts/views.py
+++ CafeManagement/acounts/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from orders.models import Order, Receipt
 from menu_items.models import MenuItem
 from django.http import Http404
+from django.contrib.auth import authenticate, login
 
 class LoginView(View):
     
@@ -34,38 +35,38 @@ class CashierView(LoginRequiredMixin, UserPassesTestMixin, View):
         ...
 
 
-# class ManagerView(LoginRequiredMixin, UserPassesTestMixin, View):
-
-#     login_url = '/login/'
-#     redirect_field_name = 'redirect_to'
-
-#     def test_func(self): 
-#         result = self.request.user.groups.filter(name="manager").exists()
-#         if not result:
-#             raise Http404
-#         return result
-
-#     def get(self, request):
-#         reciepts = Receipt.objects.all()
-#         return render(request, 'manager.html', context={"reciepts":reciepts})
-
-
-
-class ManagerView(LoginRequiredMixin, PermissionRequiredMixin, View):
+class ManagerView(LoginRequiredMixin, UserPassesTestMixin, View):
 
     login_url = '/login/'
     redirect_field_name = 'redirect_to'
 
-    def dispatch(self, *args, **kwargs):
-        if not self.request.user.is_authenticated:
-            return redirect(self.login_url)
-        elif not (
-            self.request.user.groups.filter(name="manager").exists()
-        ):
+    def test_func(self): 
+        result = self.request.user.groups.filter(name="manager").exists()
+        if not result:
             raise Http404
-        return super(ManagerView, self).dispatch(*args, **kwargs)
+        return result
 
     def get(self, request):
         reciepts = Receipt.objects.all()
         return render(request, 'manager.html', context={"reciepts":reciepts})
+
+
+
+# class ManagerView(LoginRequiredMixin, PermissionRequiredMixin, View):
+
+#     login_url = '/login/'
+#     redirect_field_name = 'redirect_to'
+
+#     def dispatch(self, *args, **kwargs):
+#         if not self.request.user.is_authenticated:
+#             return redirect(self.login_url)
+#         elif not (
+#             self.request.user.groups.filter(name="manager").exists()
+#         ):
+#             raise Http404
+#         return super(ManagerView, self).dispatch(*args, **kwargs)
+
+#     def get(self, request):
+#         reciepts = Receipt.objects.all()
+#         return render(request, 'manager.html', context={"reciepts":reciepts})
 
