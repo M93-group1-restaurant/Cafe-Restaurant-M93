b8666aa964a03ba170f3af960a2e11554bd48df7
Farzaneh_sz <f.soltanzade72@gmail.com>
2023-07-12 01:36:43 +0330

    edit form.py

--- CafeManagement/dashboard/forms.py
+++ CafeManagement/dashboard/forms.py
@@ -1,7 +1,8 @@
 from orders.models import Order
+from django import forms
 
 
 class ChangeOrderStatusForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ("serving_status")
+        fields = ("serving_status",)
